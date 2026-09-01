88ede3281900e92fc2f400027cca7ed3c9716891
kietnt <nguyentuankiet0105@gmail.com>
2026-09-01 20:25:08 +0700

    feat: add prisma serice layer

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { HealthModule } from './health/health.module.js';
+import { PrismaModule } from './prisma/prisma.module.js';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
@@ -16,6 +17,7 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
       serviceId: 'auth-admin-service',
     }),
     HealthModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
